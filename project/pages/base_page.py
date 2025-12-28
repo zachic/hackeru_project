4a9b6705da2d0af6f588fb297621a3260ec74f68
zachi cohen <zachic200897@gmail.com>
2025-12-28 18:20:25 +0200

    Create BasePage class for common page actions
    Implemented common actions for page interactions.

---
+++ project/pages/base_page.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# This is the "Parent" page that contains common actions
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def find_element(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator))
+
+    def click_element(self, locator):
+        self.find_element(locator).click()
+
+    def enter_text(self, locator, text):
+        self.find_element(locator).send_keys(text)
